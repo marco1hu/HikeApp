68ee7a3cba5c05c1ac1351bf33313c1c19a2da15
Marco <humarco1310@gmail.com>
2024-12-01 16:54:37 +0100

    Hike completed App SwiftUI

---
+++ HikeApp/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HikeApp
+//
+//  Created by Marco Hu on 21/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
